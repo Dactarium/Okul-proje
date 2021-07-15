4f94af0c3ba21a3f38b3d6cd46bab850a4822226
Eren Erdoğan <dactarium@outlook.com>
2021-07-15 23:35:03 +0300

    Added unique code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
+interface user {
+  user_code: string
+}
 
 
 @Component({
@@ -13,52 +17,65 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Bi\'Sipariş'
+  user_code = ""
   isSignedIn = false
-  firebaseErrorMsg  = "-"
+  firebaseErrorMsg = "-"
 
-  constructor(public router: Router, private authService: AuthService, public angularFireAuth: AngularFireAuth){
+  constructor(public router: Router, private authService: AuthService, public angularFireAuth: AngularFireAuth, public angularFirestore: AngularFirestore) {
   }
 
-  ngOnInit(){
-    if(localStorage.getItem('user') !== null){
-      this.isSignedIn = true
-      this.router.navigate(['siparisler'])
+  ngOnInit() {
+    if (localStorage.getItem('user') !== null) {
+      console.warn("oninit")
+      this.signedIn(false)
     }
     else
-    this.isSignedIn = false
+      this.isSignedIn = false
   }
 
-  async onSignIn(email: string, password: string){
+  async onSignIn(email: string, password: string) {
     this.authService.loginUser(email, password).then(result => {
-      if(result == null){
-        this.isSignedIn = true
-        this.router.navigate(['musteriler'])
-        window.location.reload()
-      }else if(result.isValid == false){
+      if (result == null) {
+        console.warn("onsignin")
+        this.signedIn(true)
+      } else if (result.isValid == false) {
         this.firebaseErrorMsg = result?.message
       }
     })
   }
 
-  async onSignUp(email: string, password: string){
-    this.authService.signupUser(email, password).then(result => {
-      if(result == null){
-        this.isSignedIn = true
-        this.router.navigate(['musteriler'])
-        window.location.reload()
-      }else if(result.isValid == false){
-        this.firebaseErrorMsg = result?.message
-      }
-    })
+  async onSignUp(email: string, password: string) {
+    const result = await this.authService.signupUser(email, password)
+    if (result.isValid) {
+      console.warn("onsignup")
+      this.signedIn(true)
+    } else{
+      this.firebaseErrorMsg = result?.message
+    }
   }
 
-  logout(){
+  logout() {
     this.angularFireAuth.signOut()
     localStorage.removeItem('user')
     this.router.navigate([''])
     this.isSignedIn = false;
+  }
+
+  async signedIn(reloadPage: boolean) {
+    this.isSignedIn = true
+    const user = JSON.parse(localStorage.getItem('user')!)
+    const user_ref: DocumentReference<user> = (<DocumentReference<user>>this.angularFirestore.collection("users").doc(user["email"].toLowerCase()).ref)
+    const user_data = await user_ref.get()
+    if (!user_data.exists)
+      console.log("No user document!")
+    else {
+      this.user_code = user_data.data()?.user_code!
+    }
+    this.router.navigate(['musteriler'])
+    if (reloadPage)
+      window.location.reload()
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,86 +6,124 @@ import { BehaviorSubject, Observable } from 'rxjs';
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-  userLoggedIn: boolean;      
+    userLoggedIn: boolean;
 
-  constructor(private router: Router, private angularFireAuth: AngularFireAuth, private angularFirestore: AngularFirestore) {
-      this.userLoggedIn = false;
+    constructor(private router: Router, private angularFireAuth: AngularFireAuth, private angularFirestore: AngularFirestore) {
+        this.userLoggedIn = false;
 
-      this.angularFireAuth.onAuthStateChanged((user) => {              
-          if (user) {
-              this.userLoggedIn = true;
-          } else {
-              this.userLoggedIn = false;
-          }
-      });
-  }
-
-  
-  loginUser(email: string, password: string): Promise<any> {
-    return this.angularFireAuth.signInWithEmailAndPassword(email, password)
-        .then(result => {
-            console.log('Auth Service: loginUser: success');
-            localStorage.setItem('user',JSON.stringify(result.user))
-        })
-        .catch(error => {
-            console.log('Auth Service: login error...');
-            console.log('error code', error.code);
-            console.log('error', error);
-            if (error.code)
-                return { isValid: false, message: error.message };
-            else 
-              return null
+        this.angularFireAuth.onAuthStateChanged((user) => {
+            if (user) {
+                this.userLoggedIn = true;
+            } else {
+                this.userLoggedIn = false;
+            }
         });
-  }
+    }
 
-  async signupUser(email: string, password: string) {
-    return this.angularFireAuth.createUserWithEmailAndPassword(email, password)
-        .then( result => {
-            let emailLower = email.toLowerCase();
-            
-            this.angularFirestore.collection("users").doc(emailLower).set({
-                    accountType: 'yonetici',
-                    uid: result.user?.uid,
-                    email: email,
-                    email_lower: emailLower
+
+    loginUser(email: string, password: string): Promise<any> {
+        return this.angularFireAuth.signInWithEmailAndPassword(email, password)
+            .then(result => {
+                console.log('Auth Service: loginUser: success');
+                localStorage.setItem('user', JSON.stringify(result.user))
             })
-            localStorage.setItem('user',JSON.stringify(result.user))
-            //result.user?.sendEmailVerification()                 
-        })
-        .catch(error => {
+            .catch(error => {
+                console.log('Auth Service: login error...');
+                console.log('error code', error.code);
+                console.log('error', error);
+                if (error.code)
+                    return { isValid: false, message: error.message };
+                else
+                    return null
+            });
+    }
+
+    async signupUser(email: string, password: string) {
+        try {
+            const result = await this.angularFireAuth.createUserWithEmailAndPassword(email, password)
+            let emailLower = email.toLowerCase();
+            var user_code: string = await this.generateUserCode(email)
+            await this.angularFirestore.collection("users").doc(emailLower).set({
+                accountType: 'manager',
+                uid: result.user?.uid,
+                user_code,
+                email,
+                emailLower
+            })
+            localStorage.setItem('user', JSON.stringify(result.user))
+            return { isValid: true} 
+        } catch (error) {
             console.log('Auth Service: signup error', error);
             return { isValid: false, message: error.message };
-        });
-  }
+        }
 
-  logoutUser(): Promise<void> {
-    return this.angularFireAuth.signOut()
-        .then(() => {
-            this.router.navigate(['/home']);                    
-        })
-        .catch(error => {
-            console.log('Auth Service: logout error...');
-            console.log('error code', error.code);
-            console.log('error', error);
-            return error;
-        });
-  }
-  setUserInfo(payload: object) {
-    console.log('Auth Service: saving user info...');
-    this.angularFirestore.collection('users')
-        .add(payload).then(function (res) {
-            console.log("Auth Service: setUserInfo response...")
-            console.log(res);
-        })
-  }
+    }
 
-  getCurrentUser() {
-    return this.angularFireAuth.currentUser;                                
-  }
+    logoutUser(): Promise<void> {
+        return this.angularFireAuth.signOut()
+            .then(() => {
+                this.router.navigate(['']);
+                localStorage.removeItem('user')
+            })
+            .catch(error => {
+                console.log('Auth Service: logout error...');
+                console.log('error code', error.code);
+                console.log('error', error);
+                return error;
+            });
+    }
 
-  
+    setUserInfo(payload: object) {
+        console.log('Auth Service: saving user info...');
+        this.angularFirestore.collection('users')
+            .add(payload).then(function (res) {
+                console.log("Auth Service: setUserInfo response...")
+                console.log(res);
+            })
+    }
+
+    getCurrentUser() {
+        return this.angularFireAuth.currentUser;
+    }
+
+    async generateUserCode(email: string): Promise<string> {
+        console.log("Generating user code...")
+        var user_code: string = ""
+        user_code += email.substring(0, email.lastIndexOf("@"))
+        if (user_code.length < 3)
+            user_code = (user_code + "aa").substring(0, 3)
+        else
+            user_code = user_code.substring(0, 3)
+        user_code += this.getDayOfYear().toString(20)
+
+        const result = await this.checkUserCodeExists(user_code)
+        user_code = result
+        console.log("Generated user code: ", user_code)
+
+        return user_code
+    }
+
+    getDayOfYear(): number {
+        var currentDate = new Date()
+        var firstDayOfYear = new Date(new Date().getFullYear(), 0, 0)
+        var days = Math.floor((currentDate.getTime() - firstDayOfYear.getTime()) / 1000 / 60 / 60 / 24)
+        return days
+    }
+
+    async checkUserCodeExists(user_code: string, count: number = -1): Promise<string> {
+        var checkUserCode: string = user_code
+        console.log(user_code, " ", count)
+        if (count != -1)
+            checkUserCode = user_code + count
+        const userCheck = await this.angularFirestore.collection("users").ref.where("user_code", "==", checkUserCode).get()
+        if (userCheck.empty) {
+            return checkUserCode
+        }
+        return this.checkUserCodeExists(user_code, count + 1)
+    }
 
 }
+
